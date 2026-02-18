c70fcd344247518c6c072faa8051532f5dee30e6
Malte <malte.schuette92@gmail.com>
2026-02-18 15:19:10 +0100

    Fix indentation and clean up comments in chat_view function

--- chat_app/views.py
+++ chat_app/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 
 
 def chat_view(request):
-    # if request.method == "GET":
+    if request.method == "GET":
         chats = Chat.objects.all()
         
         chat_list = []
@@ -13,12 +13,10 @@ def chat_view(request):
             chat_list.append({
                 "name": c.name,
                 "message": c.message,
-                "created_at": str(c.created_at) # Datum als Text umwandeln
+                "created_at": str(c.created_at)
             })
         
-        # Die Liste in die JsonResponse packen
         return JsonResponse({"results": chat_list})
 
-    # if request.method == "Post":
-    #     print("Das hier ist POST")
-    #     return HttpResponse("das hier ist post")
+    if request.method == "Post":
+        print("Das hier ist POST")
